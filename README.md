abbd2ff1327ebe8ef3b8647fb28623a494daddee
dschmidt <dschmidt@paloaltonetworks.com>
2023-11-19 17:08:35 -0500

    Initial import

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# illuminate-workshop-repository-12072023
+# illuminate_chicago_12072023
 Repository for the Prisma Cloud / Illuminate Chicago Workshop on December 7, 2023
---
+++ tf/illuminate_s3_bucket.tf
@@ -0,0 +1,25 @@
+resource "aws_s3_bucket" "illuminate_bucket" {
+  bucket        = "illuminate-workshop-bucket-chicago-12072023"
+
+  tags = {
+    Repalce_this_tag = "Palo Alto Networks - Prisma Cloud"
+    Environment = "Illuminate Chicago Workshop 12072023"
+  }
+
+  force_destroy = true
+}
+
+resource "aws_s3_bucket_versioning" "s3_versioning_for_illuminate_bucket" {
+  bucket = aws_s3_bucket.illuminate_bucket.id
+  versioning_configuration {
+    status = "Suspended"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "illuminate_bucket_access_policy" {
+  bucket = aws_s3_bucket.illuminate_bucket.id
+  block_public_acls   = false
+  block_public_policy = false
+  restrict_public_buckets = false
+  ignore_public_acls = false
+}
